2cfe0d721e10d0f899d876d3636c4a097e7b1ae2
mightcell <139125732+ijusthaveto@users.noreply.github.com>
2023-12-12 16:21:57 +0800

    fix: Resolve frontend-backend parameter mismatch bug

--- exam-backend/src/main/java/me/ijusthaveto/exam/controller/AdminController.java
+++ exam-backend/src/main/java/me/ijusthaveto/exam/controller/AdminController.java
@@ -2,13 +2,16 @@ package me.ijusthaveto.exam.controller;
 
 import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
+import lombok.extern.slf4j.Slf4j;
 import me.ijusthaveto.exam.common.BaseResponse;
 import me.ijusthaveto.exam.common.ResultUtils;
+import me.ijusthaveto.exam.domain.dto.AdminLoginDto;
 import me.ijusthaveto.exam.service.AdminService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("/admin")
@@ -17,8 +20,8 @@ public class AdminController {
     private AdminService adminService;
 
     @PostMapping("/login")
-    public BaseResponse<SaTokenInfo> login(@RequestBody String secretCode) {
-        adminService.login(secretCode);
+    public BaseResponse<SaTokenInfo> login(@RequestBody AdminLoginDto dto) {
+        adminService.login(dto.getSecretCode());
         return ResultUtils.success(StpUtil.getTokenInfo());
     }
 }
---
+++ exam-backend/src/main/java/me/ijusthaveto/exam/domain/dto/AdminLoginDto.java
@@ -0,0 +1,8 @@
+package me.ijusthaveto.exam.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class AdminLoginDto {
+    private String secretCode;
+}
