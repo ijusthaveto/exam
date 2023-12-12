c4c4ce358b1b0f850ef35ce3af4e3f9e37a086ad
mightcell <139125732+ijusthaveto@users.noreply.github.com>
2023-12-12 14:45:44 +0800

    feat: Display question data
    Rendered question data obtained from the backend on the frontend.

--- exam-frontend/src/views/User/components/Exam.vue
+++ exam-frontend/src/views/User/components/Exam.vue
@@ -19,8 +19,21 @@
       </el-header>
       <el-main>
         <el-scrollbar height="650px">
-          <div v-for="item in questionList" :key="item.questionId" class="scrollbar-demo-item">
-            <div class=''></div>
+          <div class="question-container">
+            <h1 class="exam-title" style="text-align: center">Exam 1</h1>
+            <h2>0x01. Single Choice</h2>
+            <div v-for="(item, index) in singleList" :key="item.questionId">
+              <p :id="'question' + (index + 1)">
+                {{ index + 1 }}.{{ item.questionContent }}
+              </p>
+              <el-radio-group>
+                <el-radio :label="2" v-for="index in JSON.parse(item.options)" :key="index">{{ index }}.{{ JSON.parse(item.options).index }}</el-radio>
+              </el-radio-group>
+            </div>
+            <h2>0x02. Multiple Choice</h2>
+            <MultipleChoice :data="mutipleList" />
+            <h2>0x03. True or False</h2>
+            <TrueOrFalse :data="boolList" />
           </div>
         </el-scrollbar>
       </el-main>
@@ -32,6 +45,7 @@
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import httpInstance from '@/utils/http.js'
+import { computed } from 'vue';
 
 const route = useRoute()
 
@@ -41,12 +55,24 @@ const questionList = ref([])
 
 const getQuestionListByExamId = async () => {
   const res = await httpInstance.get(`/exam/${examId}`)
+  console.log(res.data)
   questionList.value = res.data
 }
 
+const singleList = computed(() => {
+  return questionList.value.filter(item => item.questionType === 'Single Choice')
+})
+
+const mutipleList = computed(() => {
+  return questionList.value.filter(item => item.questionType === 'Multiple Choice')
+})
+
+const boolList = computed(() => {
+  return questionList.value.filter(item => item.questionType === 'True/False')
+})
+
 onMounted(() => {
   getQuestionListByExamId()
-  console.log(examId)
 })
 </script>
 
@@ -55,19 +81,23 @@ onMounted(() => {
   color: white;
   height: 100vh;
 }
+
 el-container {
   display: flex;
   flex-direction: column;
   height: 100%;
 }
+
 .nav {
   flex: 0 0 auto;
   padding-top: 20px;
   background-color: #24292f;
 }
+
 el-main {
   flex: 1 0 auto;
 }
+
 .scrollbar-demo-item {
   display: flex;
   align-items: center;
@@ -76,7 +106,6 @@ el-main {
   margin: 10px;
   text-align: center;
   border-radius: 4px;
-  background: var(--el-color-primary-light-9);
   color: var(--el-color-primary);
 }
 </style>
