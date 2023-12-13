7c685a038ca54425b5e5eebdf6a849138c70ecef
mightcell <139125732+ijusthaveto@users.noreply.github.com>
2023-12-13 22:54:12 +0800

    chore: Generate basic structure for Task

---
+++ exam-backend/src/main/java/me/ijusthaveto/exam/service/TaskService.java
@@ -0,0 +1,13 @@
+package me.ijusthaveto.exam.service;
+
+import me.ijusthaveto.exam.domain.Task;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 修雯天
+* @description 针对表【task】的数据库操作Service
+* @createDate 2023-12-13 22:52:38
+*/
+public interface TaskService extends IService<Task> {
+
+}
