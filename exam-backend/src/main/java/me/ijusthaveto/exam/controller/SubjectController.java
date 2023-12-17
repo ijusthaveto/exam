4478c7817cb68fe922762b7fb9c8b2024dcdb8bb
mightcell <139125732+ijusthaveto@users.noreply.github.com>
2023-12-17 18:17:43 +0800

    feat(api): Add endpoint for querying list of subjects

--- exam-backend/src/main/java/me/ijusthaveto/exam/controller/SubjectController.java
+++ exam-backend/src/main/java/me/ijusthaveto/exam/controller/SubjectController.java
@@ -1,5 +1,6 @@
 package me.ijusthaveto.exam.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import me.ijusthaveto.exam.common.BaseResponse;
 import me.ijusthaveto.exam.common.ResultUtils;
 import me.ijusthaveto.exam.domain.Subject;
@@ -7,6 +8,8 @@ import me.ijusthaveto.exam.service.SubjectService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+
+import java.util.List;
 
 import static me.ijusthaveto.exam.constant.ResultConstant.ADD_SUBJECT_SUCCESS;
 
@@ -31,5 +34,16 @@ public class SubjectController {
     public BaseResponse<String> save(@RequestBody Subject subject) {
         subjectService.save(subject);
         return ResultUtils.success(ADD_SUBJECT_SUCCESS);
+    }
+
+    /**
+     * 查询学科列表
+     * @return
+     */
+    public BaseResponse<List<Subject>> list() {
+        LambdaQueryWrapper<Subject> wrapper = new LambdaQueryWrapper<>();
+        wrapper.select(Subject::getSubjectId, Subject::getSubjectName);
+        List<Subject> list = subjectService.list(wrapper);
+        return ResultUtils.success(list);
     }
 }
