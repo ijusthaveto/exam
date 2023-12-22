ed4e43d4334c3c08cd34ed42ea894480983499e1
mightcell <139125732+ijusthaveto@users.noreply.github.com>
2023-12-22 22:24:17 +0800

    feat: Introduce component styles for element plus

--- exam-frontend/src/main.js
+++ exam-frontend/src/main.js
@@ -1,6 +1,7 @@
 import '@/styles/common.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
